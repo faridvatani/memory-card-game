27243ee402204eda3cfdd90dd460baff728cb559
Farid Vatani <faridvatani@gmail.com>
2024-08-29 22:07:15 +0330

    refactor: Update image file extensions to webp

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,8 @@
-export { default as donut } from "@/assets/images/donut.png";
-export { default as frenchFries } from "@/assets/images/french-fries.png";
-export { default as friedEgg } from "@/assets/images/fried-egg.png";
-export { default as hotDog } from "@/assets/images/hot-dog.png";
-export { default as iceCream } from "@/assets/images/ice-cream.png";
-export { default as pizza } from "@/assets/images/pizza.png";
-export { default as sandwich } from "@/assets/images/sandwich.png";
-export { default as taco } from "@/assets/images/taco.png";
+export { default as donut } from "@/assets/images/donut.webp";
+export { default as frenchFries } from "@/assets/images/french-fries.webp";
+export { default as friedEgg } from "@/assets/images/fried-egg.webp";
+export { default as hotDog } from "@/assets/images/hot-dog.webp";
+export { default as iceCream } from "@/assets/images/ice-cream.webp";
+export { default as pizza } from "@/assets/images/pizza.webp";
+export { default as sandwich } from "@/assets/images/sandwich.webp";
+export { default as taco } from "@/assets/images/taco.webp";
